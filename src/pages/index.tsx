ac5989590db7e6c5a4ba3e0aa0151f138f5f00fd
Duffyboyo <mail@duffyboyo.com>
2020-10-21 15:59:39 +0100

    Minor frontpage changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,10 @@ export default function Index() {
       <div className="container">
         <div>
           <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
+            Hi, Im <span className="fancy">Adam</span>.
           </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
+          <span className="handle">@duffyboyo</span>
+          <h2>Freelance Software and Web Developer.</h2>
           <SocialList />
         </div>
       </div>
